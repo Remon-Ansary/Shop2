8f3776d89836680838ace27aff79fdf1cb35245e
Remon-Ansary <rasel15-10514@diu.edu.bd>
2022-02-08 10:54:28 +0600

    Used async/await in actions

--- src/store/index.js
+++ src/store/index.js
@@ -19,54 +19,35 @@ const getters = {
 
 //to handle actions
 const actions = {
-  allProducts({ commit }) {
+  async allProducts({ commit }) {
     if (this.state.products == null) {
-      axios.get("https://fakestoreapi.com/products").then((response) => {
-        commit("SET_PRODUCTS", response.data), (this.loading = true)
-      })
+      const response = await axios.get("https://fakestoreapi.com/products")
+      commit("SET_PRODUCTS", response.data), (this.loading = true)
     }
   },
-  singleProduct({ commit }, productId) {
-    axios
-      .get(`https://fakestoreapi.com/products/${productId}`)
-      .then((response) => {
-        commit("SET_SINGLE_PRODUCTS", response.data)
-      })
+  async singleProduct({ commit }, productId) {
+    const response = await axios.get(
+      `https://fakestoreapi.com/products/${productId}`
+    )
+
+    commit("SET_SINGLE_PRODUCTS", response.data)
   },
 
   async updateProduct({ commit }, payload) {
     console.log(payload.id)
-    // let uri = `https://fakestoreapi.com/products//${payload.id}`
-    // axios.put(uri, payload).then((response) => {
-    // console.log(response.data)
+
     commit("updateProduct", payload)
-    // })
   },
 
   deleteProduct({ commit }, id) {
-    // let uri = "https://fakestoreapi.com/products/" + id
-    // axios.delete(uri).then((response) => {
-    //   console.log(response.data)
-    // alert("Deleted  " + response.data.title)
     commit("DELETE_PRODUCT", id)
-
-    // })
   },
 
-  // async createProduct({ commit }, title, price, image) {
-  //   let uri = "https://fakestoreapi.com/products"
-  //   axios.post(uri, { title, price, image }).then((response) => {
-  //     console.log(response.data)
-  //     commit("createProduct", response.data)
-  //   })
-  // },
   async createProduct({ commit }, payload) {
-    console.log(payload)
     let uri = "https://fakestoreapi.com/products"
-    axios.post(uri, payload).then((response) => {
-      console.log(response.data)
-      commit("createProduct", response.data)
-    })
+    const response = await axios.post(uri, payload)
+
+    commit("createProduct", response.data)
   },
 }
 
